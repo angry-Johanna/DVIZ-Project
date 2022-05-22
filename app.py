d86a922c2e4507df72b9c70a24553921bc7e68fc
Johanna <johanna.zoe.koch@bluewin.ch>
2022-05-22 16:16:13 +0200

    Add first serious plot

--- app.py
+++ app.py
@@ -17,26 +17,28 @@ path = 'https://raw.githubusercontent.com/angry-Johanna/DVIZ-Project/main/data_c
 
 # Import Kriterien Gemuesekauf Data
 # bio
-KG_bio_imm = pd.read_csv(path + "Kriterien_Gemuesekauf/bio/bio_immer.CSV")
-KG_bio_mei = pd.read_csv(path + "Kriterien_Gemuesekauf/bio/bio_meistens.CSV")
-KG_bio_gel = pd.read_csv(path + "Kriterien_Gemuesekauf/bio/bio_gelegentlich.CSV")
-KG_bio_sel = pd.read_csv(path + "Kriterien_Gemuesekauf/bio/bio_selten.CSV")
-KG_bio_nie = pd.read_csv(path + "Kriterien_Gemuesekauf/bio/bio_nie.CSV")
+KG_bio_imm = pd.read_csv(path + "Kriterien_Gemuesekauf/bio/bio_immer.CSV",sep=";")
+KG_bio_mei = pd.read_csv(path + "Kriterien_Gemuesekauf/bio/bio_meistens.CSV",sep=";")
+KG_bio_gel = pd.read_csv(path + "Kriterien_Gemuesekauf/bio/bio_gelegentlich.CSV",sep=";")
+KG_bio_sel = pd.read_csv(path + "Kriterien_Gemuesekauf/bio/bio_selten.CSV",sep=";")
+KG_bio_nie = pd.read_csv(path + "Kriterien_Gemuesekauf/bio/bio_nie.CSV",sep=";")
+KG_bio_comp = pd.read_csv(path + "Kriterien_Gemuesekauf/bio/bio_complete.CSV",sep=";")
 
 # regional
-KG_reg_imm = pd.read_csv(path + "Kriterien_Gemuesekauf/regional/reg_immer.CSV")
-KG_reg_mei = pd.read_csv(path + "Kriterien_Gemuesekauf/regional/reg_meistens.CSV")
-KG_reg_gel = pd.read_csv(path + "Kriterien_Gemuesekauf/regional/reg_gelegentlich.CSV")
-KG_reg_sel = pd.read_csv(path + "Kriterien_Gemuesekauf/regional/reg_selten.CSV")
-KG_reg_nie = pd.read_csv(path + "Kriterien_Gemuesekauf/regional/reg_nie.CSV")
+KG_reg_imm = pd.read_csv(path + "Kriterien_Gemuesekauf/regional/reg_immer.CSV",sep=";")
+KG_reg_mei = pd.read_csv(path + "Kriterien_Gemuesekauf/regional/reg_meistens.CSV",sep=";")
+KG_reg_gel = pd.read_csv(path + "Kriterien_Gemuesekauf/regional/reg_gelegentlich.CSV",sep=";")
+KG_reg_sel = pd.read_csv(path + "Kriterien_Gemuesekauf/regional/reg_selten.CSV",sep=";")
+KG_reg_nie = pd.read_csv(path + "Kriterien_Gemuesekauf/regional/reg_nie.CSV",sep=";")
+KG_reg_comp = pd.read_csv(path + "Kriterien_Gemuesekauf/regional/reg_complete.CSV",sep=";")
 
 # seasonal
-KG_sai_imm = pd.read_csv(path + "Kriterien_Gemuesekauf/saisonal/sais_immer.CSV")
-KG_sai_mei = pd.read_csv(path + "Kriterien_Gemuesekauf/saisonal/sais_meistens.CSV")
-KG_sai_gel = pd.read_csv(path + "Kriterien_Gemuesekauf/saisonal/sais_gelegentlich.CSV")
-KG_sai_sel = pd.read_csv(path + "Kriterien_Gemuesekauf/saisonal/sais_selten.CSV")
-KG_sai_nie = pd.read_csv(path + "Kriterien_Gemuesekauf/saisonal/sais_nie.CSV")
-
+KG_sai_imm = pd.read_csv(path + "Kriterien_Gemuesekauf/saisonal/sais_immer.CSV",sep=";")
+KG_sai_mei = pd.read_csv(path + "Kriterien_Gemuesekauf/saisonal/sais_meistens.CSV",sep=";")
+KG_sai_gel = pd.read_csv(path + "Kriterien_Gemuesekauf/saisonal/sais_gelegentlich.CSV",sep=";")
+KG_sai_sel = pd.read_csv(path + "Kriterien_Gemuesekauf/saisonal/sais_selten.CSV",sep=";")
+KG_sai_nie = pd.read_csv(path + "Kriterien_Gemuesekauf/saisonal/sais_nie.CSV",sep=";")
+KG_sai_comp = pd.read_csv(path + "Kriterien_Gemuesekauf/saisonal/sais_complete.CSV",sep=";")
 
 
 # ---- #
@@ -46,10 +48,6 @@ KG_sai_nie = pd.read_csv(path + "Kriterien_Gemuesekauf/saisonal/sais_nie.CSV")
 
 
 # Test-dataset
-data = dict(
-    character=["Eve", "Cain", "Seth", "Enos", "Noam", "Abel", "Awan", "Enoch", "Azula"],
-    parent=["", "Eve", "Eve", "Seth", "Seth", "Eve", "Eve", "Awan", "Eve" ],
-    value=[10, 14, 12, 10, 2, 6, 6, 4, 4])
 
 
 
@@ -62,13 +60,6 @@ data = dict(
 # ------- #
 # FIGURES #
 # ------- #
-# Testfigure
-fig_test = px.sunburst(
-    data,
-    names='character',
-    parents='parent',
-    values='value',
-)
 
 
 # ---------- #
@@ -79,16 +70,14 @@ app = dash.Dash(__name__)
 server = app.server
 
 app.layout = html.Div([
-    
-    # Test-Figure
-    html.Div([
-        html.Label("Test-Figure", style={'font-size': 'medium'}),
-        html.Br(),
-        html.Label('Click on it to know more!', style={'font-size':'9px'}),
-        html.Br(), 
-        html.Br(), 
-        dcc.Graph(figure=fig_test)
-    ])
+    html.H4('Shopping Organic'),
+    dcc.Graph(id="graph-organic"),
+    dcc.Checklist(
+        id="checklist",
+        options=["Always", "Often", "Sometimes","Rarely","Never"],
+        value=["Always", "Never"],
+        inline=True
+    )
     
 ])
 
@@ -97,6 +86,19 @@ app.layout = html.Div([
 # --------- #
 # CALLBACKS #
 # --------- #
+
+@app.callback(
+    Output("graph-organic", "figure"), 
+    Input("checklist", "value"))
+def update_line_chart(cat):
+    df = KG_bio_comp
+    mask = df.Category.isin(cat)
+    fig = px.line(df[mask], 
+        x="Year", y=["Frau","Mann"])
+    fig.update_xaxes(type='category')
+    fig.update_yaxes(range=[0,100])
+    return fig
+
 
 
 
