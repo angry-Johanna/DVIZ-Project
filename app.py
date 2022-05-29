8632f1de3e4bb7492dd02d225de84f1e76a9c0f2
Johanna <johanna.zoe.koch@bluewin.ch>
2022-05-29 14:22:09 +0200

    Remove no longer needed charts and data import

--- app.py
+++ app.py
@@ -15,32 +15,8 @@ import plotly.express as px
 path = 'https://raw.githubusercontent.com/angry-Johanna/DVIZ-Project/main/data_csv/'
 
 
-# Import Kriterien Gemuesekauf Data
-# bio
-KG_bio_imm = pd.read_csv(path + "Kriterien_Gemuesekauf/bio/bio_immer.CSV",sep=";")
-KG_bio_mei = pd.read_csv(path + "Kriterien_Gemuesekauf/bio/bio_meistens.CSV",sep=";")
-KG_bio_gel = pd.read_csv(path + "Kriterien_Gemuesekauf/bio/bio_gelegentlich.CSV",sep=";")
-KG_bio_sel = pd.read_csv(path + "Kriterien_Gemuesekauf/bio/bio_selten.CSV",sep=";")
-KG_bio_nie = pd.read_csv(path + "Kriterien_Gemuesekauf/bio/bio_nie.CSV",sep=";")
+# Import Kriterien Gemuesekauf organic Data 
 KG_bio_comp = pd.read_csv(path + "Kriterien_Gemuesekauf/bio/bio_complete.CSV",sep=";")
-
-# regional
-KG_reg_imm = pd.read_csv(path + "Kriterien_Gemuesekauf/regional/reg_immer.CSV",sep=";")
-KG_reg_mei = pd.read_csv(path + "Kriterien_Gemuesekauf/regional/reg_meistens.CSV",sep=";")
-KG_reg_gel = pd.read_csv(path + "Kriterien_Gemuesekauf/regional/reg_gelegentlich.CSV",sep=";")
-KG_reg_sel = pd.read_csv(path + "Kriterien_Gemuesekauf/regional/reg_selten.CSV",sep=";")
-KG_reg_nie = pd.read_csv(path + "Kriterien_Gemuesekauf/regional/reg_nie.CSV",sep=";")
-KG_reg_comp = pd.read_csv(path + "Kriterien_Gemuesekauf/regional/reg_complete.CSV",sep=";")
-
-# seasonal
-KG_sai_imm = pd.read_csv(path + "Kriterien_Gemuesekauf/saisonal/sais_immer.CSV",sep=";")
-KG_sai_mei = pd.read_csv(path + "Kriterien_Gemuesekauf/saisonal/sais_meistens.CSV",sep=";")
-KG_sai_gel = pd.read_csv(path + "Kriterien_Gemuesekauf/saisonal/sais_gelegentlich.CSV",sep=";")
-KG_sai_sel = pd.read_csv(path + "Kriterien_Gemuesekauf/saisonal/sais_selten.CSV",sep=";")
-KG_sai_nie = pd.read_csv(path + "Kriterien_Gemuesekauf/saisonal/sais_nie.CSV",sep=";")
-KG_sai_comp = pd.read_csv(path + "Kriterien_Gemuesekauf/saisonal/sais_complete.CSV",sep=";")
-
-
 
 # import type of food eaten per person/year
 ft_per_pers = pd.read_csv(path + "csv_Nahrungsmittelverbrauch_Bilanz_pro_Pers_new_sunburst.csv",sep=";")
@@ -125,111 +101,10 @@ fig_line_org.update_layout(
 )
 fig_line_org.update_yaxes(range=[0,50])
 
-# ------- LINE CHART SEASONAL ---------
-# initial chart
-fig_line_sea = go.Figure()
 
-# drop duplicates from category
-KG_sai_comp_cat = KG_sai_comp.Category.drop_duplicates()
+# ------- BAR CHART MEAT CONSUMPTION ---------
 
-# color_map to color lines
-color_map = {"Always":"Green","Often":"Green","Sometimes":"Yellow","Rarely":"Orange","Never":"Red"}
-
-# add lines for each category
-for index, value in KG_sai_comp_cat.items():
-    # mask to draw a line for each category individually
-    mask = KG_sai_comp.Category.isin([value])
-    # create names for lines
-    name_men = "Men buying "+value.lower()+" seasonal"
-    name_women = "Women buying "+value.lower()+" seasonal"
-    # add line for men
-    fig_line_sea.add_trace(go.Scatter(
-    x=KG_sai_comp[mask].Year, 
-    y=KG_sai_comp[mask].Mann,
-    name = name_men,
-    mode = 'lines',
-    text = value,
-    marker = {'color' : color_map[value]},
-    line = {'dash': 'dot'}
-    ))
-    # add line for women
-    fig_line_sea.add_trace(go.Scatter(
-    x=KG_sai_comp[mask].Year, 
-    y=KG_sai_comp[mask].Frau,
-    name = name_women,
-    mode = 'lines',
-    text = value,
-    marker = {'color' : color_map[value]}
-    ))
-
-# make it pretty
-fig_line_sea.update_layout(
-    title="Shopping seasonal",
-    xaxis_title="Year",
-    yaxis_title="Percentage",
-    legend_title="Legend Title",
-    font=dict(
-        family="Courier New, monospace",
-        size=15,
-        color="RebeccaPurple"
-    )
-)
-fig_line_sea.update_yaxes(range=[0,50])
-
-# ------- LINE CHART REGIONAL ---------
-# initial chart
-fig_line_reg = go.Figure()
-
-# drop duplicates from category
-KG_reg_comp_cat = KG_reg_comp.Category.drop_duplicates()
-
-# color_map to color lines
-color_map = {"Always":"Green","Often":"Green","Sometimes":"Yellow","Rarely":"Orange","Never":"Red"}
-
-# add lines for each category
-for index, value in KG_reg_comp_cat.items():
-    # mask to draw a line for each category individually
-    mask = KG_reg_comp.Category.isin([value])
-    # create names for lines
-    name_men = "Men buying "+value.lower()+" regional"
-    name_women = "Women buying "+value.lower()+" regional"
-    # add line for men
-    fig_line_reg.add_trace(go.Scatter(
-    x=KG_reg_comp[mask].Year, 
-    y=KG_reg_comp[mask].Mann,
-    name = name_men,
-    mode = 'lines',
-    text = value,
-    marker = {'color' : color_map[value]},
-    line = {'dash': 'dot'}
-    ))
-    # add line for women
-    fig_line_reg.add_trace(go.Scatter(
-    x=KG_reg_comp[mask].Year, 
-    y=KG_reg_comp[mask].Frau,
-    name = name_women,
-    mode = 'lines',
-    text = value,
-    marker = {'color' : color_map[value]}
-    ))
-
-# make it pretty
-fig_line_reg.update_layout(
-    title="Shopping regional",
-    xaxis_title="Year",
-    yaxis_title="Percentage",
-    legend_title="Legend Title",
-    font=dict(
-        family="Courier New, monospace",
-        size=15,
-        color="RebeccaPurple"
-    )
-)
-fig_line_reg.update_yaxes(range=[0,50])
 fig_barchart = px.bar(comp_meat, x = "Year", y=["Cow", "Veal", "Pig","Chicken", "Others"])
-
-# ------- MEAT CONSUMPTION
-
 
 
 
@@ -249,22 +124,6 @@ app.layout = html.Div([
         html.Br(), 
         html.Br(), 
         dcc.Graph(figure=fig_line_org)
-    ]),
-    
-    # Buying seasonal
-    html.Div([
-        html.H1("Shopping seasonal groceries"),
-        html.Br(), 
-        html.Br(), 
-        dcc.Graph(figure=fig_line_sea)
-    ]),
-    
-    # Buying regional
-    html.Div([
-        html.H1("Shopping regional groceries"),
-        html.Br(), 
-        html.Br(), 
-        dcc.Graph(figure=fig_line_reg)
     ]),
     
     # sunburst
